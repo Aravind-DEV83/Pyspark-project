189e95f256c8b6fb86f068b6b21660e792f98707
Aravind Jarpala <aravind_jarpala@Aravinds-MacBook-Pro.local>
2024-09-11 14:56:45 +0530

    Added feature CI workflow 11

--- scripts/spark-submit.sh
+++ scripts/spark-submit.sh
@@ -11,3 +11,9 @@ gcloud dataproc jobs submit pyspark gs://pyspark-files-adev-spark/gcs_to_bq/job.
 
 # python source/dataProcessingCode/src/main/job.py --input_location gs://input-source-adev-spark/uber_5.csv --project_id adev-spark --temp_bucket temp_gcs_adev-spark --output_table temp.trip_data
 
+gcloud iam service-accounts add-iam-policy-binding "ci-cd-service-account@adev-spark.iam.gserviceaccount.com" \
+  --project="adev-spark" \
+  --role="roles/iam.workloadIdentityUser" \
+  --member="principalSet://iam.googleapis.com/projects/878514427384/locations/global/workloadIdentityPools/githubactions/attribute.repository/Aravind-DEV83/Pyspark-project"
+
+  https://iam.googleapis.com/projects/878514427384/locations/global/workloadIdentityPools/githubactions/providers/github
